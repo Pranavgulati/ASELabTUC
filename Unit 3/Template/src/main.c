88ac98193d135d51577cf81df5c7b92926a4eefe
Pranavgulati <pranavgulati95@gmail.com>
2017-11-29 22:13:34 +0100

    Monitoring ECU done

--- Unit 3/Template/src/main.c
+++ Unit 3/Template/src/main.c
@@ -11,8 +11,28 @@ typedef unsigned char byte;
 /********************************************************************
 *                          global variables                         *
 ********************************************************************/
+unsigned char isError=1;//initially ECU1 is not found
+unsigned char isAlive=1;
+unsigned char isStabilityPresent=0;
 
-msgPacket packet1,packet2,packet3;
+
+msgPacket lightPacket;
+msgPacket doorPacket;
+msgPacket fuelWarningPacket;
+msgPacket engCoolPacket;
+msgPacket alivePacket;
+msgPacket errorPacket;
+
+
+#define LIGHTBUF 0
+#define DOORBUF 1
+#define FUELBUF 2
+#define ENGCOOLBUF 3
+#define ALIVEBUF 4
+#define ERRORBUF 5
+
+#define LEFT_DOOR 0
+#define RIGHT_DOOR 1
 
 /********************************************************************
 *                    _____  ___  ___   ___                          *
@@ -30,6 +50,8 @@ void main(void)
 {
     /* board initialization */
     Init();
+    showData(ADC_0.CDR[2].B.CDATA);//light sensor
+    showData(ADC_0.CDR[4].B.CDATA);//potentiometer
     PIT_ConfigureTimer(0,100);
     PIT_ConfigureTimer(1,200);
     PIT_StartTimer(0);
@@ -42,9 +64,27 @@ void main(void)
     *                                                                   *
     * Assign the message ID's to the corresponding packets here         *
     ********************************************************************/  
-    packet1.standardId=75; 
-    packet2.standardId=2; 
-    packet3.standardId=2; 
+    lightPacket.standardId=0x101; 
+    lightPacket.dataLength=2;
+
+    doorPacket.standardId=0x102; 
+    doorPacket.dataLength=1;
+
+    fuelWarningPacket.standardId=0x103; 
+    fuelWarningPacket.dataLength=1;
+    fuelWarningPacket.data=1;
+
+    engCoolPacket.standardId=0x104; 
+    engCoolPacket.dataLength=1;
+    engCoolPacket.data=1;
+
+    alivePacket.standardId=0x105; 
+    alivePacket.dataLength=0;
+    alivePacket.data=0;
+    
+    errorPacket.standardId=0x106; 
+    errorPacket.dataLength=0;
+    errorPacket.data=0;
 
     /* turn off leds */
     LED0 = 1;
@@ -89,9 +129,36 @@ void PITCHANNEL0(void)
     /* your own code above! */
     PIT.CH[0].TFLG.B.TIF = 1;
 }
-
+unsigned char interruptCounter=0;
 void PITCHANNEL1(void)
-{
+{ //called every 200ms
+
+    lightPacket.data=ADC_0.CDR[2].B.CDATA;
+    CAN_Write(LIGHTBUF,lightPacket)
+
+    if(SIU.GPDI[44].R==0){
+        doorPacket.data &= ~(1<<LEFT_DOOR);
+    }else{
+        doorPacket.data |= (1<<LEFT_DOOR);
+    }
+    if(SIU.GPDI[4].R==0){
+        doorPacket.data &= ~(1<<RIGHT_DOOR);
+    }else{
+        doorPacket.data |= (1<<RIGHT_DOOR);
+    }
+    CAN_Write(DOORBUF,doorPacket);
+
+    if(isError){CAN_Write(ERRORBUF,errorPacket); LED7=~LED7;}
+    if(isAlive){CAN_Write(ALIVEBUF,alivePacket);}
+
+    interruptCounter++;
+    if(interruptCounter%2==0){
+        //executed every 400ms
+        CAN_Write(FUELBUF,fuelWarningPacket);
+        CAN_Write(ENGCOOLBUF,engCoolPacket);
+        isError=1;// turn on the error so that ecu1 can turn it off
+    }
+
     /* your own code above! */
     PIT.CH[1].TFLG.B.TIF = 1;
 }
@@ -111,10 +178,34 @@ void CANMB0003(void)
     CAN_0.IFRL.B.BUF02I = 1;
     CAN_0.IFRL.B.BUF03I = 1;
 }
+#define FUELLEVEL_ID 0x401
+#define COOLANT_ID 0x501
+#define COOL_THRESH 922
+#define FUEL_THRESH 102
+#define ECU1_ALIVE 0x204
 
 void onMessageReceived(msgPacket inMsgPacket){
 //use the inMsgPacket to do the intended function with the packet
+    switch(inMsgPacket.standardId){
+        case FUELLEVEL_ID:
+            if(inMsgPacket.data<=FUEL_THRESH){
+                fuelWarningPacket.data=0;
+            }else{
+                fuelWarningPacket.data=1;
+            }
+        break;
+        case COOLANT_ID:
+            if(inMsgPacket.data>=COOL_THRESH){
+                engCoolPacket.data=0;
+            }else{
+                engCoolPacket.data=1;
+            }
+        break;
+        case ECU1_ALIVE:
+            isError=0;
+        break;
 
+    }
 }
 
 void CANMB0407(void)
